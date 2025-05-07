adac8dcb263b3d229c6c420a6b89d967a1c90e0c
Ramchandra prasad <34053563+ram0555@users.noreply.github.com>
2025-05-07 21:05:12 +0530

    Delete api directory

--- api/index.js
+++
@@ -1,9 +0,0 @@
-// Vercel serverless function
-const { createServer } = require('http');
-const express = require('express');
-require('../server/index.js');
-
-const app = express();
-const server = createServer(app);
-
-module.exports = server;
